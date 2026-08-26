2c28b4c75ba467563305fd1fc0ce666ab21df6ed
aayushi <e1591061@u.nus.edu>
2026-08-26 08:06:36 +0800

    Add greeting and exit

--- src/main/java/Todd.java
+++ src/main/java/Todd.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Todd {
     static void main(String[] args) {
         String banner = "   ______          __     __\n"
@@ -6,5 +7,10 @@ public class Todd {
                 + "  / / / /_/ / /_/ / /_/ /  \n"
                 + " /_/  \\____/\\__,_/\\__,_/   \n";
         System.out.println(banner);
+        System.out.println("Hello There! I'm Todd, a NPC Chatbot :P\nWhat can I do for you today?");
+        Scanner sc = new Scanner(System.in);
+        if (sc.nextLine().equals("bye")) {
+            System.out.println("Noo don't go, come back. Ok fine bye. See you soon.");
+        }
     }
 }
