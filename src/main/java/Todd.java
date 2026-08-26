c35b6a93fec0579b4f15931aac15a662f93e2dda
aayushi <e1591061@u.nus.edu>
2026-08-26 12:03:11 +0800

    Add list

--- src/main/java/Todd.java
+++ src/main/java/Todd.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Todd {
     static void main(String[] args) {
@@ -7,6 +8,7 @@ public class Todd {
                 + "   / / / __ \\/ __  / __  / \n"
                 + "  / / / /_/ / /_/ / /_/ /  \n"
                 + " /_/  \\____/\\__,_/\\__,_/   \n";
+        ArrayList<String> list = new ArrayList<>();
 
         System.out.println(banner);
         System.out.println("Hello There! I'm Todd, a NPC Chatbot :P\nWhat can I do for you today?");
@@ -14,12 +16,34 @@ public class Todd {
         Scanner sc = new Scanner(System.in);
         String txt = sc.nextLine();
 
-        while (!txt.equals("bye")) {
-            System.out.println(line);
-            System.out.println("\t" + txt);
-            System.out.println(line);
-            txt = sc.nextLine();
+        while (true) {
+            switch (txt) {
+                case "bye":
+                    System.out.println("Noo don't go, come back. Ok fine bye. See you soon.");
+                    return;
+                case "list":
+                    if (list.isEmpty()) {
+                        System.out.println("\t" + line);
+                        System.out.println("\tThe list is empty");
+                        System.out.println("\t" + line);
+                    } else {
+                        int i = 1;
+                        for (String s : list) {
+                            System.out.println("\t" + line);
+                            System.out.println("\t" + i + ". " + s);
+                            System.out.println("\t" + line);
+                            i++;
+                        }
+                    }
+                    txt = sc.nextLine();
+                    break;
+                default:
+                    System.out.println("\t" + line);
+                    list.add(txt);
+                    System.out.println("\t" + "added: " + txt);
+                    System.out.println("\t" + line);
+                    txt = sc.nextLine();
+            }
         }
-        System.out.println("Noo don't go, come back. Ok fine bye. See you soon.");
     }
 }
