3ef02b54de2fac2cbdbccd36326d5407c08a9417
aayushi <e1591061@u.nus.edu>
2026-08-26 11:13:25 +0800

    Add echo

--- src/main/java/Todd.java
+++ src/main/java/Todd.java
@@ -1,16 +1,25 @@
 import java.util.Scanner;
 public class Todd {
     static void main(String[] args) {
+        String line = "____________________________________________________________";
         String banner = "   ______          __     __\n"
                 + "  /_  __/___  ____/ /____/ /\n"
                 + "   / / / __ \\/ __  / __  / \n"
                 + "  / / / /_/ / /_/ / /_/ /  \n"
                 + " /_/  \\____/\\__,_/\\__,_/   \n";
+
         System.out.println(banner);
         System.out.println("Hello There! I'm Todd, a NPC Chatbot :P\nWhat can I do for you today?");
+
         Scanner sc = new Scanner(System.in);
-        if (sc.nextLine().equals("bye")) {
-            System.out.println("Noo don't go, come back. Ok fine bye. See you soon.");
+        String txt = sc.nextLine();
+
+        while (!txt.equals("bye")) {
+            System.out.println(line);
+            System.out.println("\t" + txt);
+            System.out.println(line);
+            txt = sc.nextLine();
         }
+        System.out.println("Noo don't go, come back. Ok fine bye. See you soon.");
     }
 }
