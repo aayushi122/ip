a5d32c2216cc7aed9482ce97bd401b2a6fad85b4
aayushi <e1591061@u.nus.edu>
2026-08-27 21:39:07 +0800

    Add mark as done

--- src/main/java/Todd.java
+++ src/main/java/Todd.java
@@ -1,14 +1,16 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Todd {
-    static void main(String[] args) {
-        String line = "____________________________________________________________";
+    public static void main(String[] args) {
+        String line = "\t____________________________________________________________";
         String banner = "   ______          __     __\n"
                 + "  /_  __/___  ____/ /____/ /\n"
                 + "   / / / __ \\/ __  / __  / \n"
                 + "  / / / /_/ / /_/ / /_/ /  \n"
                 + " /_/  \\____/\\__,_/\\__,_/   \n";
         ArrayList<String> list = new ArrayList<>();
+        ArrayList<Boolean> done = new ArrayList<>();
 
         System.out.println(banner);
         System.out.println("Hello There! I'm Todd, a NPC Chatbot :P\nWhat can I do for you today?");
@@ -17,32 +19,52 @@ public class Todd {
         String txt = sc.nextLine();
 
         while (true) {
-            switch (txt) {
-                case "bye":
-                    System.out.println("Noo don't go, come back. Ok fine bye. See you soon.");
-                    return;
-                case "list":
-                    if (list.isEmpty()) {
-                        System.out.println("\t" + line);
-                        System.out.println("\tThe list is empty");
-                        System.out.println("\t" + line);
-                    } else {
-                        int i = 1;
-                        for (String s : list) {
-                            System.out.println("\t" + line);
-                            System.out.println("\t" + i + ". " + s);
-                            System.out.println("\t" + line);
-                            i++;
-                        }
+            if (txt.equals("bye")) {
+                System.out.println("Noo don't go, come back. Ok fine bye. See you soon.");
+                return;
+
+            } else if (txt.equals("list")) {
+                System.out.println(line);
+                System.out.println("\t Here are the tasks in your list:");
+                for (int i = 0; i < list.size(); i++) {
+                    String status = done.get(i) ? "X" : " ";
+                    System.out.println("\t  " + (i + 1) + ". [" + status + "] " + list.get(i));
+                }
+                System.out.println(line);
+                txt = sc.nextLine();
+
+            } else if (txt.startsWith("mark ")) {
+                String numberPart = txt.substring(5);
+                boolean isValidNumber = !numberPart.isEmpty();
+                for (int i = 0; i < numberPart.length(); i++) {
+                    if (!Character.isDigit(numberPart.charAt(i))) {
+                        isValidNumber = false;
                     }
-                    txt = sc.nextLine();
-                    break;
-                default:
-                    System.out.println("\t" + line);
-                    list.add(txt);
-                    System.out.println("\t" + "added: " + txt);
-                    System.out.println("\t" + line);
-                    txt = sc.nextLine();
+                }
+
+                System.out.println(line);
+                if (!isValidNumber) {
+                    System.out.println("\t Enter the task number pls");
+                } else {
+                    int index = Integer.parseInt(numberPart) - 1;
+                    if (index < 0 || index >= list.size()) {
+                        System.out.println("\t That task number doesn't exist!");
+                    } else {
+                        done.set(index, true);
+                        System.out.println("\t Hooray! You are on fire! Task crossed off.");
+                        System.out.println("\t    [X] " + list.get(index));
+                    }
+                }
+                System.out.println(line);
+                txt = sc.nextLine();
+
+            } else {
+                System.out.println(line);
+                list.add(txt);
+                done.add(false);
+                System.out.println("\t added: " + txt);
+                System.out.println(line);
+                txt = sc.nextLine();
             }
         }
     }
