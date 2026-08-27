a9885822d8c8f036ba514b7cabdc0b9be9f37ab8
aayushi <e1591061@u.nus.edu>
2026-08-27 22:38:16 +0800

    Update Todd to include todo, deadlines and events

--- src/main/java/Todd.java
+++ src/main/java/Todd.java
@@ -26,8 +26,7 @@ public class Todd {
                 System.out.println(line);
                 System.out.println("\t Here are the tasks in your list:");
                 for (int i = 0; i < list.size(); i++) {
-                    Task t = list.get(i);
-                    System.out.println("\t  " + (i + 1) + ". [" + t.getStatusIcon() + "] " + t.getDescription());
+                    System.out.println("\t  " + (i + 1) + "." + list.get(i));
                 }
                 System.out.println(line);
                 txt = sc.nextLine();
@@ -53,7 +52,7 @@ public class Todd {
                     } else {
                         list.get(index).markAsDone();
                         System.out.println("\t Hooray! You are on fire! Task crossed off.");
-                        System.out.println("\t    [X] " + list.get(index).getDescription());
+                        System.out.println("\t    " + list.get(index));
                     }
                 }
                 System.out.println(line);
@@ -80,16 +79,56 @@ public class Todd {
                     } else {
                         list.get(index).markAsUndone();
                         System.out.println("\t Oh no! Okay unmarked.");
-                        System.out.println("\t    [ ] " + list.get(index).getDescription());
+                        System.out.println("\t    " + list.get(index));
                     }
                 }
                 System.out.println(line);
                 txt = sc.nextLine();
 
+            } else if (txt.startsWith("todo ")) {
+                String description = txt.substring(5);
+                Task newTask = new Todo(description);
+                list.add(newTask);
+                System.out.println(line);
+                System.out.println("\t Got it. I've added this task:");
+                System.out.println("\t   " + newTask);
+                System.out.println("\t Now you have " + list.size() + " tasks in the list.");
+                System.out.println(line);
+                txt = sc.nextLine();
+
+            } else if (txt.startsWith("deadline ")) {
+                String rest = txt.substring(9);
+                String[] parts = rest.split(" /by ", 2);
+                String description = parts[0];
+                String by = parts.length > 1 ? parts[1] : "";
+                Task newTask = new Deadline(description, by);
+                list.add(newTask);
+                System.out.println(line);
+                System.out.println("\t Got it. I've added this task:");
+                System.out.println("\t   " + newTask);
+                System.out.println("\t Now you have " + list.size() + " tasks in the list.");
+                System.out.println(line);
+                txt = sc.nextLine();
+
+            } else if (txt.startsWith("event ")) {
+                String rest = txt.substring(6);
+                String[] fromSplit = rest.split(" /from ", 2);
+                String description = fromSplit[0];
+                String[] toSplit = fromSplit.length > 1 ? fromSplit[1].split(" /to ", 2) : new String[]{""};
+                String from = toSplit[0];
+                String to = toSplit.length > 1 ? toSplit[1] : "";
+                Task newTask = new Event(description, from, to);
+                list.add(newTask);
+                System.out.println(line);
+                System.out.println("\t Got it. I've added this task:");
+                System.out.println("\t   " + newTask);
+                System.out.println("\t Now you have " + list.size() + " tasks in the list.");
+                System.out.println(line);
+                txt = sc.nextLine();
+
             } else {
                 System.out.println(line);
-                list.add(new Task(txt));
-                System.out.println("\t added: " + txt);
+                System.out.println("\t I don't understand that command.");
                 System.out.println(line);
                 txt = sc.nextLine();
             }
